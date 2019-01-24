425070bc002f1ec1e60b102137003b00d541c5aa
jonas <jonas-gruempel@gmx.de>
2019-01-24 22:28:58 +0100

    added function to change variable value 'CHANGE NICK OF ... TO ...'

--- core/PokeCodeInterpreter.js
+++ core/PokeCodeInterpreter.js
@@ -4,6 +4,8 @@ const PokeCodeInterpreter = {
 
     Variables: {},
 
+    activeLine: 0,
+
     init: () => {
         // do some initiation stuff here
     },
@@ -11,8 +13,10 @@ const PokeCodeInterpreter = {
     runCode: (_code) => {   
         if (Array.isArray(_code)) {
             try {
+                PokeCodeInterpreter.activeLine = 0;
                 PokeCodeInterpreter.clearConsole();
                 _code.forEach(st => {
+                    PokeCodeInterpreter.activeLine++;
                     PokeCodeInterpreter.interpretStatement(st);
                 });
             } catch (er) {
@@ -30,28 +34,61 @@ const PokeCodeInterpreter = {
     },
 
     interpretStatement: (_statement) => {
-        if (_statement.match(/A WILD.*?APPEARED/i)) {
-            let name = _statement.split(/A WILD/i)[1];
-            name = name.split(/APPEARED/i)[0];
-            name = name.replace(/\s/g, "");
-            var value;
-            if (_statement.match(/ITS NAME IS/i)) {
-                value = _statement.split(/ITS NAME IS/i)[1];
-            }
-            PokeCodeInterpreter.createVariable(name, value);
-        } else if (_statement.match(/ANNOYING DIALOGUE/i)) {
-            let variable = _statement.split(/ANNOYING DIALOGUE/i)[1].replace(/\s/g, "");
-            PokeCodeInterpreter.writeStringToConsole(PokeCodeInterpreter.getVariableValue(variable));
-        } else if (_statement.match(/RAPE B/i) || _statement.match(/RAPE A/i)) {
+        // Data decleration
+        if (_statement.match(/A\s*WILD.*?APPEARED/i)) {
+            PokeCodeInterpreter.interpretDataDeclerationStatement(_statement);
+        // write to console statement
+        } else if (_statement.match(/ANNOYING\s*DIALOGUE/i)) {
+            PokeCodeInterpreter.interpreteConsoleWriteStatement(_statement);
+        // change variable value
+        } else if (_statement.match(/CHANGE\s*NICK\s*OF/i)) {
+            PokeCodeInterpreter.interpreteChangeValueStatement(_statement);
+        // clear console
+        } else if (_statement.match(/RAPE\s*[AB]/i)) {
             PokeCodeInterpreter.clearConsole();
         }
     },
 
+    interpretDataDeclerationStatement: (_statement) => {
+        // get name value from data decleration statement
+        let name = _statement.split(/A\s*WILD/i)[1];
+        name = name.split(/APPEARED/i)[0];
+        name = name.replace(/\s/g, "");
+        // get value val from data decleration statement
+        var value;
+        if (_statement.match(/ITS\s*NAME\s*IS/i)) {
+            value = _statement.split(/ITS\s*NAME\s*IS/i)[1];
+        }
+        // call internal core function
+        PokeCodeInterpreter.createVariable(name, value);
+    },
+
+    interpreteConsoleWriteStatement: (_statement) => {
+        // get message value from statement
+        let variable = _statement.split(/ANNOYING\s*DIALOGUE/i)[1].replace(/\s/g, "");
+        // call internal core function
+        PokeCodeInterpreter.writeStringToConsole(PokeCodeInterpreter.getVariableValue(variable));
+    },
+
+    interpreteChangeValueStatement: (_statement) => {
+        // get name value from statement
+        let name = _statement.split(/CHANGE\s*NICK\s*OF/i)[1];
+        name = name.split(/TO/i)[0];
+        name = name.replace(/\s/g, "");
+        // get value val from statement
+        let value = _statement.split(/TO/i)[1];
+        value = value.trim();
+        // call internal core function
+        PokeCodeInterpreter.setVariableValue(name, value);
+    },
+
     addConsole: (_htmlId) => {
+        // adding a console (has to be a div-container)
         this.PokeConsole = document.getElementById(_htmlId);
     },
 
     createVariable: (_name, _value) => {
+        // creating an variable
         if (!PokeCodeInterpreter.checkVariableExist()) {
             PokeCodeInterpreter.Variables[_name] = _value;
         } else {
@@ -60,6 +97,7 @@ const PokeCodeInterpreter = {
     },
 
     checkVariableExist: (_name) => {
+        // check if the variable is allready created
         let val = PokeCodeInterpreter.Variables[_name];
         let r;
         val==undefined ? r=false : r=true;
@@ -67,8 +105,18 @@ const PokeCodeInterpreter = {
     },
 
     getVariableValue: (_name) => {
+        // returns the value of an created variable
         if (PokeCodeInterpreter.checkVariableExist(_name)) {
             return PokeCodeInterpreter.Variables[_name];
+        } else {
+            PokeCodeInterpreter.writeErrorToConsole(`Variable ${_name} hasn't been created yet!`);
+        }
+    },
+
+    setVariableValue: (_name, _value) => {
+        // set the value of an variable
+        if (PokeCodeInterpreter.checkVariableExist(_name)) {
+            PokeCodeInterpreter.Variables[_name] = _value;
         } else {
             PokeCodeInterpreter.writeErrorToConsole(`Variable ${_name} hasn't been created yet!`);
         }
@@ -84,7 +132,9 @@ const PokeCodeInterpreter = {
     },
 
     writeErrorToConsole: (_string) => {
-        let string = `<SPAN style="color:red">${_string}</SPAN>`;
+        // writing an error message to console and start an 
+        // core error to stop execution
+        let string = `<SPAN style="color:red">${_string} (Line: ${PokeCodeInterpreter.activeLine})</SPAN>`;
         PokeCodeInterpreter.writeStringToConsole(string);
         throw new Error("An error occured, see PokeCode console");
     },
